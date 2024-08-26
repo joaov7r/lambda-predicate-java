bf514ce7e14a494d4e6692cb57b24a8badc6ecbd
João Vitor <jvc.lopes@outlook.com>
2024-08-26 15:07:47 -0300

    Implementing class with Predicate

--- src/app/Program.java
+++ src/app/Program.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Locale;
 
 import entities.Product;
+import util.ProductPredicate;
 
 public class Program {
 
@@ -18,7 +19,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(p -> p.getPrice() >= 100);
+		list.removeIf(new ProductPredicate());
 		
 		for(Product p : list) {
 			System.out.println(p);
