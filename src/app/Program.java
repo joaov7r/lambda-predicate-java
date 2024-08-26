d2acd40ae7f18dedaf08ede913f8407800838383
João Vitor <jvc.lopes@outlook.com>
2024-08-26 15:17:14 -0300

    Inline lambda expression

--- src/app/Program.java
+++ src/app/Program.java
@@ -19,9 +19,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Predicate<Product> pred = p -> p.getPrice() >= 100.00;
-		
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= 100.00);
 		
 		for(Product p : list) {
 			System.out.println(p);
